2146f80b4f99788f0da59bc0c737aad4caaef531
Sean T. Kahn <seantkahn75@gmail.com>
2023-12-10 23:31:57 -0500

    project update

--- ios/App/App/public/assets/ios.transition-legacy-edd88e04.js
+++ ios/App/App/public/assets/ios.transition-legacy-edd88e04.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a09b2fa7.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-5db0233c.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- ios/App/App/public/assets/md.transition-legacy-8de16736.js
+++ ios/App/App/public/assets/md.transition-legacy-8de16736.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a09b2fa7.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-5db0233c.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
