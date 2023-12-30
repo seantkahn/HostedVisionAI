0733f1b69b56e268f16325083a55677b1e600acb
Sean T. Kahn <seantkahn75@gmail.com>
2023-12-29 22:25:52 -0500

    Added data collection disclaimer to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,8 +33,11 @@ const Home: React.FC = () => {
         <h1 className="welcome-title">Welcome to the Vision AI App</h1>
 
         <p className="app-description">
-          An Artificial Intelligence Based Near Vision Tester. <br/> Providing
-          accurate and reliable at home vision testing.
+          An Artificial Intelligence Based Near Vision Tester. <br/> 
+          Providing accurate and reliable at home vision testing.<br/>
+          <br/> 
+          Camera and microphone access required for testing.<br/> 
+          Personal data is never collected nor stored.
         </p>
         <Button buttonText="Continue" onClickAction={goToTermsPage} />
       </IonContent>
